e53680131789b4894d6cb00a778a0fdfa28fab59
artemius466 <81032239+artemius466@users.noreply.github.com>
2025-10-08 20:52:54 +0700

    an attempt to fix building after make clean

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@ codesign --remove-signature "${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}"
 
 rm -rf $(BUILT_PRODUCTS_DIR)
 
+cat > ./levels/level_rules.mk << 'EOF'
+#define STUB_LEVEL(_0, _1, _2, _3, _4, _5, _6, _7, _8)
+#define DEFINE_LEVEL(_0, _1, _2, folder, texturebin, _5, _6, _7, _8, _9, _10) $(eval $(call level_rules,folder,texturebin))
+
+#include "levels/level_defines.h"
+
+#undef STUB_LEVEL
+#undef DEFINE_LEVEL
+EOF
+
 #touch "levels/level_rules.mk"
 #echo "#define STUB_LEVEL(_0, _1, _2, _3, _4, _5, _6, _7, _8) \
 ##define DEFINE_LEVEL(_0, _1, _2, folder, texturebin, _5, _6, _7, _8, _9, _10) $(eval \ $(call level_rules,folder,texturebin)) \
